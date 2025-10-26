998c29f7e0dc7fbe7d798408081e494e4c19aafa
Emily <emily@anxiety.rip>
2025-10-26 21:08:48 +1100

    ahhh diddy blud

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests, json, os, validators
 from bs4 import BeautifulSoup
 import urllib.parse, urllib.request
 
+from tag import tagFolder
+
 URL = input("project name ? : ")
 if not validators.url(URL):
     print("URL is NOT valid. Try again.")
@@ -15,20 +17,32 @@ script_tag = soup.find_all('script')[2]
 data = script_tag.text.split("window.__remixContext = ", 1)[-1].rsplit(';', 1)[0]
 data = json.loads(data)
 
-downloadable_urls = []
+trackDetails = []
 
+# Extract downloadable signed URLs and details from API into dictionaries
 for track in data['state']['loaderData']['routes/library.project.$projectSlug']['project']['tracks']:
+    artwork_url = data['state']['loaderData']['routes/library.project.$projectSlug']['project']['project']['artwork_signed_url']
     file_dir = urllib.parse.quote_plus(track['audio_fallback_url'].split('/', 7)[-1])
     downloadURL = API.format(MUSIC_URL=file_dir)
     album_title = data['state']['loaderData']['routes/library.project.$projectSlug']['project']['project']['title']
-    downloadable_urls.append([downloadURL, track['title'], album_title])
+    trackDetail = {
+        'url': downloadURL,
+        'title': track['title'],
+        'album': album_title,
+        'albumArt': artwork_url
+    }
+    trackDetails.append(trackDetail)
 
 newpath = f'./{album_title}' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
-for url in downloadable_urls:
-    r = requests.get(url[0])
-    print(r.json()['url'])
-    urllib.request.urlretrieve(r.json()['url'].replace("https", "http"), f"./{url[2]}/{url[1]}.mp3")
+#Download each file
+
+# for details in trackDetails:
+#     r = requests.get(details['url'])
+#     print("Downloading ", details['title'], "...")
+#     urllib.request.urlretrieve(r.json()['url'].replace("https", "http"), f"./{details['album']}/{details['title']}.mp3")
     
+print("Tagging each File.")    
+tagFolder(f"./{album_title}", trackDetails)
---
+++ tag.py
@@ -0,0 +1,36 @@
+# load the libraries that we'll use  
+from mutagen.mp3 import MP3  
+from mutagen.easyid3 import EasyID3  
+import mutagen.id3  
+from mutagen.id3 import ID3, APIC, PictureType
+import glob, os, mimetypes
+import numpy as np
+import urllib.request, urllib.parse
+
+
+def tagFolder(path, trackDetails):
+    # extract the file names (including folders)  
+    # for the mp3s in the album  
+    filez = glob.glob(path+"/*.mp3")  
+    # print the first element of filez:  
+    albumArtName = urllib.parse.urlparse(trackDetails[0]['albumArt'])[2].split('/')[-1]
+    if not os.path.exists(path+"/"+albumArtName):
+        urllib.request.urlretrieve(trackDetails[0]['albumArt'], path+"/"+albumArtName)
+
+    image_mime_type = mimetypes.guess_file_type(path+"/"+albumArtName)[0]
+
+    with open(path+"/"+albumArtName, 'rb') as f:
+        image_data = f.read()
+
+    for file in filez:
+        print("Tagging", file)
+        tags = ID3(file)
+        tags.setall('APIC', [APIC(
+            mime=image_mime_type,
+            type=PictureType.COVER_FRONT,
+            data=image_data
+        )])
+        tags.save(v2_version=3)
+
+if __name__ == "__main__":
+    tagFolder("./within", None)
